bc7a89c6d7c3804d6f013360814e18cac5980f4c
Nemmz <130888338+Nemmz@users.noreply.github.com>
2023-11-09 11:47:53 -0700

    Update Project.cpp
    Changed to C++ syntax

--- Project.cpp
+++ Project.cpp
@@ -1,42 +1,39 @@
-/************
+/******************************
 * Author: Calyn, Lauren, and Issac
 * CS 225 Project
 * Purpose: This file asks the user if they want to play mastermind, then runs the MasterMind function if they do.
-* Based off of Version 1.7 from Apr 1, 2023 
+* Based off of Version 1.7 from Apr 1, 2023
 * Our versions: 2.0 October 31, 2023
-*************/
-#include "PatternGen.h" 
+******************************/
+#include "PatternGen.h"
 #include "headers.h"
-// Prototypes of functions used
-int MasterMind();
-char numbers_to_colors(int);
+
 int main()
 {
-   char ans1, ans2;
-   int x; // for loop veriable
-   FILE* f =  fopen("rules.txt" , "r");   
-   char rules[500];
-    
-   cout<<("Do you wanna play Mastermind? (y/n) ");
-   cin>>ans1;   
-   cout<<(" ")<<endl;
+    char ans1, ans2;
 
-   if (ans1 == 'y'|| ans1=='Y') {
-      for (x=0;x<1; x++){
-        fgets(rules, 500, f);             //prints the rules from the text file
-        printf("%s" , rules);
-      }
-  
-  cout<<(" ")<<endl;
-   cout<<"Are you ready to play? (y/n) ")<<endl;
-   cin>>ans2;
-   //play game
-      while (ans2 == 'y' || ans2=='Y') { 
-      MasterMind();
-   // prompt user if they want to play agian
-      printf("\nDo you want to play again? (y/n) ");  
-      cin>>ans2;
-      }
-      }
-   printf("Have a nice day!\n");
+    cout<<("Do you wanna play Mastermind? (y/n) ");
+    cin>>ans1;
+    cout<<(" ")<<endl;
+
+    if (ans1 == 'y'|| ans1=='Y') {
+        ifstream rulesFile("rules.txt"); // changes if the rules files is a different name.
+        while(rulesFile.eof()){
+            string rules;
+            rulesFile >> rules;
+        }
+
+        cout<<(" ")<<endl;
+        cout<<"Are you ready to play? (y/n) " <<endl;
+        cin>>ans2;
+        //play game
+        while (ans2 == 'y' || ans2=='Y') {
+            MasterMind();
+            // prompt user if they want to play again
+            cout << endl << "\nDo you want to play again? (y/n) ";
+            cin>>ans2;
+        }
+    }
+    cout << "Have a nice day!" << endl;
 }
+
