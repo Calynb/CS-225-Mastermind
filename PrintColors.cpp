b1c57a36c54cb98052bd77901b643bb2af22868d
Nemmz <130888338+Nemmz@users.noreply.github.com>
2023-11-09 11:26:51 -0700

    Update PrintColors.cpp
    Changed the color output to C++ syntax.

--- PrintColors.cpp
+++ PrintColors.cpp
@@ -1,44 +1,48 @@
 /*****************************
 * Author: Monica Hyndman
+* Edited By: Isaac Jarrells
 * Purpose: To print letters with corresponding colors
 * Resources: Professor Marriot
 *  For colors: https://gist.github.com/RabaDabaDoba/145049536f815903c79944599c6f952a
 * Version: 1.1 April 4,2023
 ******************************/
+
 #include "headers.h"
 #include "PatternGen.h"
 
 void PrintColors(char rn)
 {
-  
-// print R in bold red 
-  if (rn=='R'){
-    printf(" \e[1;31m R\e[0m");
-  }
-  
-  
-// print B in bold blue
-  else if (rn=='B'){
-    printf(" \e[1;34m B\e[0m");
-  }
-  
-// print G in bold green
-  else if (rn=='G'){
-    printf(" \e[1;32m G\e[0m");
-  }
-  
-// print Y in bold yellow
-  else if (rn=='Y'){
-    printf(" \e[1;33m Y\e[0m");
-  }
 
-// print C in bold cyan
-  else if (rn=='C'){
-    printf(" \e[1;36m C\e[0m");
-  }
-  
-// print P in bold magenta
-  else if (rn=='M'){
-    printf(" \e[;35m M\e[0m");
-  }
-  }
+// print R in bright red
+    if (rn=='R'){
+        std::cout << "\033[91m" << "R" << "\033[0m";
+    }
+
+
+// print B in bright blue
+    else if (rn=='B'){
+        std::cout << "\033[94m" << "B" << "\033[0m";
+    }
+
+// print G in bright green
+    else if (rn=='G'){
+        std::cout << "\033[92m" << "R" << "\033[0m";
+    }
+
+// print Y in bright yellow
+    else if (rn=='Y'){
+        std::cout << "\033[91m" << "R" << "\033[0m";
+    }
+
+// print C in bright cyan
+    else if (rn=='C'){
+        std::cout << "\033[96m" << "R" << "\033[0m";
+    }
+
+// print P in bright magenta
+    else if (rn=='M'){
+        std::cout << "\033[95m" << "R" << "\033[0m";
+    }
+}
+
+
