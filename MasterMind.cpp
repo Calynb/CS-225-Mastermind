b50990a7f2a26cfc982923261cffbe7177f0628f
LaurenB25 <127565908+LaurenB25@users.noreply.github.com>
2023-11-09 11:19:55 -0700

    Update MasterMind.cpp
    Nov 9 2023

--- MasterMind.cpp
+++ MasterMind.cpp
@@ -10,72 +10,71 @@
 #include "PatternGen.h"
 using namespace std;
 
-// prototypes
-void PrintColors();
 
 int MasterMind()
+
 {
-   rand_nums rn;
-   rn=PatternGenerator();
-   char guess_1, guess_2, guess_3, guess_4;
-   int count=0;
-   char congrats[] = {'C','o','n','g','r','a','t','u','l','a','t','i','o','n','s','!'}; 
-   int i=0;
- 
-  //printf("%d %d %d %d", rn.num_1, rn.num_2, rn.num_3, rn.num_4);
-  cout<<"Type your 4 guesses: ")<<endl;
-  scanf(" %c%*c%c%*c%c%*c%c" , &guess_1, &guess_2, &guess_3, &guess_4);
-  //printf("GUESS 1=%c GUESS2=%c GUESS3=%c GUESS4=%c \n", guess1, guess2, guess3, guess4);
-     
-  while ((guess_1 != rn.num_1) || (guess_2 != rn.num_2) || (guess_3 != rn.num_3) || (guess_4 != rn.num_4)) {
-// create if loop, if the answer is correct, print 1, if incorrect, print 0
-    if (guess_1 == rn.num_1){
-	cout<<"1 "<<endl;
+    rand_nums rn;
+    rn = PatternGenerator();
+    char guess_1, guess_2, guess_3, guess_4;
+    int count = 0;
+    char congrats[] = { 'C','o','n','g','r','a','t','u','l','a','t','i','o','n','s','!' };
+    int i = 0;
+
+    //printf("%d %d %d %d", rn.num_1, rn.num_2, rn.num_3, rn.num_4);
+    cout << "Type your 4 guesses: " << endl;
+    cin >> guess_1 >> guess_2 >> guess_3 >> guess_4;
+    //printf("GUESS 1=%c GUESS2=%c GUESS3=%c GUESS4=%c \n", guess1, guess2, guess3, guess4);
+
+    while ((guess_1 != rn.num_1) || (guess_2 != rn.num_2) || (guess_3 != rn.num_3) || (guess_4 != rn.num_4)) {
+        // create if loop, if the answer is correct, print 1, if incorrect, print 0
+        if (guess_1 == rn.num_1) {
+            cout << "1 " << endl;
+        }
+        if (guess_1 != rn.num_1) {
+            cout << "0 " << endl;
+        }
+        if (guess_2 == rn.num_2) {
+            cout << "1 " << endl;
+        }
+        if (guess_2 != rn.num_2) {
+            cout << "0 " << endl;
+        }
+        if (guess_3 == rn.num_3) {
+            cout << "1 " << endl;
+        }
+        if (guess_3 != rn.num_3) {
+            cout << "0 " << endl;
+        }
+        if (guess_4 == rn.num_4) {
+            cout << "1 " << endl;
+        }
+        if (guess_4 != rn.num_4) {
+            cout << "0 " << endl;
+        }
+        count = count + 1;
+        cout << "Count: " << count << endl;
+
+        if (count >= 12) {                          // If the user cannot guess the code in 12 turns, the game ends
+            cout << "Sorry, you are out of turns. The pattern is " << endl;
+            PrintColors(rn.num_1);
+            PrintColors(rn.num_2);
+            PrintColors(rn.num_3);
+            PrintColors(rn.num_4);
+            break;
+        }
+        cout << "Type your 4 guesses: " << endl;
+        cin >> guess_1 >> guess_2 >> guess_3 >> guess_4;
     }
-    if (guess_1 != rn.num_1){
-	cout<<"0 "<<endl;
+    if (count <= 12 && (guess_1 == rn.num_1) && (guess_2 == rn.num_2) && (guess_3 == rn.num_3) && (guess_4 == rn.num_4)) {
+        while (congrats[i]) {
+            printf("%c", congrats[i++]);
+        }
+        cout << " You are correct! " << endl;
+        cout << "\nThe pattern is " << endl;
+        PrintColors(rn.num_1);
+        PrintColors(rn.num_2);
+        PrintColors(rn.num_3);
+        PrintColors(rn.num_4);
     }
-    if (guess_2 == rn.num_2){
-	cout<<"1 "<<endl;
-    }
-    if (guess_2 != rn.num_2){
-	cout<<"0 "<<endl;
-    }
-    if (guess_3 == rn.num_3){
-	cout<<"1 "<<endl;
-    }
-    if (guess_3 != rn.num_3){
-	cout<<"0 "<<endl;
-    }
-    if (guess_4 == rn.num_4){
-	cout<<"1 "<<endl;
-    }
-    if (guess_4 != rn.num_4){
-	cout<<"0 "<<endl;
-    }
-  count = count + 1;
-  cout<<"Count: "<<count<<endl;
-  
- if (count >= 12){                          // If the user cannot guess the code in 12 turns, the game ends
-	 cout<<"Sorry, you are out of turns. The pattern is "<<endl;
-    PrintColors(rn.num_1);
-    PrintColors(rn.num_2);
-    PrintColors(rn.num_3);
-    PrintColors(rn.num_4);
-	 break;
-    }
-  cout<<"Type your 4 guesses: "<<endl;
-  scanf(" %c%*c%c%*c%c%*c%c", &guess_1, &guess_2, &guess_3, &guess_4);
- }
-  if (count <= 12 && (guess_1==rn.num_1) && (guess_2==rn.num_2) && (guess_3==rn.num_3) && (guess_4==rn.num_4) ){
-    while( congrats[i] ) { 
-        printf( "%c", congrats[i++] );
-        }     
-    cout<<" You are correct! ")<<endl;
-    cout<<"\nThe pattern is "<<endl;
-    PrintColors(rn.num_1);
-    PrintColors(rn.num_2);
-    PrintColors(rn.num_3);
-    PrintColors(rn.num_4);    
-  }  
 }
