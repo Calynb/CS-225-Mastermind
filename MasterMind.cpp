f45baa5f4f326f3b321fe987b820de5d2844fed9
LaurenB25 <127565908+LaurenB25@users.noreply.github.com>
2023-11-11 16:42:19 -0700

    Update MasterMind.cpp

--- MasterMind.cpp
+++ MasterMind.cpp
@@ -6,75 +6,65 @@
 *  Previous: Version 1.7  Apr 4, 2023
 * Ours: Version 2.0 October 31, 2023
 *************/
+#include <iostream>
+#include <vector>
 #include "headers.h"
 #include "PatternGen.h"
-using namespace std;
+#include "Scoreboard.h"  
 
-
-int MasterMind()
-
-{
+int MasterMind() {
     rand_nums rn;
     rn = PatternGenerator();
-    char guess_1, guess_2, guess_3, guess_4;
+    const int codeLength = 4;
+    char guesses[codeLength];
     int count = 0;
-    vector<char> congrats = {'C','o','n','g','r','a','t','u','l','a','t','i','o','n','s','!' };
+    vector<char> congrats = {'C', 'o', 'n', 'g', 'r', 'a', 't', 'u', 'l', 'a', 't', 'i', 'o', 'n', 's', '!'};
 
-    //printf("%d %d %d %d", rn.num_1, rn.num_2, rn.num_3, rn.num_4);
-    cout << "Type your 4 guesses: " << endl;
-    cin >> guess_1 >> guess_2 >> guess_3 >> guess_4;
-    //printf("GUESS 1=%c GUESS2=%c GUESS3=%c GUESS4=%c \n", guess1, guess2, guess3, guess4);
+    while (count < 12) {
+        cout << "Type your 4 guesses: ";
+        for (int i = 0; i < codeLength; ++i) {
+            cin >> guesses[i];
+        }
 
-    while ((guess_1 != rn.num_1) || (guess_2 != rn.num_2) || (guess_3 != rn.num_3) || (guess_4 != rn.num_4)) {
-        // create if loop, if the answer is correct, print 1, if incorrect, print 0
-        if (guess_1 == rn.num_1) {
-            cout << "1 " << endl;
+        for (int i = 0; i < codeLength; ++i) {
+            if (guesses[i] == rn.num_1) {
+                cout << "1 ";
+            } else if (guesses[i] == rn.num_2 || guesses[i] == rn.num_3 || guesses[i] == rn.num_4) {
+                cout << "0 ";
+            } else {
+                cout << "x ";
+            }
         }
-        if (guess_1 != rn.num_1) {
-            cout << "0 " << endl;
-        }
-        if (guess_2 == rn.num_2) {
-            cout << "1 " << endl;
-        }
-        if (guess_2 != rn.num_2) {
-            cout << "0 " << endl;
-        }
-        if (guess_3 == rn.num_3) {
-            cout << "1 " << endl;
-        }
-        if (guess_3 != rn.num_3) {
-            cout << "0 " << endl;
-        }
-        if (guess_4 == rn.num_4) {
-            cout << "1 " << endl;
-        }
-        if (guess_4 != rn.num_4) {
-            cout << "0 " << endl;
-        }
-        count = count + 1;
-        cout << "Count: " << count << endl;
 
-        if (count >= 12) {                          // If the user cannot guess the code in 12 turns, the game ends
-            cout << "Sorry, you are out of turns. The pattern is " << endl;
+        cout << endl;
+
+        count++;
+
+        if (count >= 12) {
+            cout << "Sorry, you are out of turns. The pattern is: " << endl;
             PrintColors(rn.num_1);
             PrintColors(rn.num_2);
             PrintColors(rn.num_3);
             PrintColors(rn.num_4);
             break;
         }
-        cout << "Type your 4 guesses: " << endl;
-        cin >> guess_1 >> guess_2 >> guess_3 >> guess_4;
+
+        if (guesses[0] == rn.num_1 && guesses[1] == rn.num_2 && guesses[2] == rn.num_3 && guesses[3] == rn.num_4) {
+            cout << "Congratulations! You are correct!" << endl;
+            cout << "The pattern is " << endl;
+            PrintColors(rn.num_1);
+            PrintColors(rn.num_2);
+            PrintColors(rn.num_3);
+            PrintColors(rn.num_4);
+            return 0;
+        }
     }
-    
-    if (count <= 12 && (guess_1 == rn.num_1) && (guess_2 == rn.num_2) && (guess_3 == rn.num_3) && (guess_4 == rn.num_4)) {
-        for (int i = 0; i < congrats.size(); ++i) {
-        cout << congrats[i] << " ";
-    }
-        cout << " You are correct! " << endl;
-        cout << "\nThe pattern is " << endl;
-        PrintColors(rn.num_1);
-        PrintColors(rn.num_2);
-        PrintColors(rn.num_3);
-        PrintColors(rn.num_4);
-    }
+
+    return 0;
+}
+
+int main() {
+    MasterMind();
+    Scoreboard();
+    return 0;
 }
