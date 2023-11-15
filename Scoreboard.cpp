5f46982a12e9c10c3d8901147fa14e6f441359cf
LaurenB25 <127565908+LaurenB25@users.noreply.github.com>
2023-11-14 17:33:06 -0700

    Update Scoreboard.cpp

--- Scoreboard.cpp
+++ Scoreboard.cpp
@@ -3,8 +3,8 @@
 #include "PatternGen.h"
 #include "Scoreboard.h"  
 
-#define ROWS 9
-#define COLS 20
+#define ROWS 4
+#define COLS 4
 
 void print_array(char lines[ROWS][COLS]) {
     for (int x = 0; x < ROWS; x++) {
