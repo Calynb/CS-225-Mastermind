3b814b964a17a4db599ea8e2b617daaf52be2aef
LaurenB25 <127565908+LaurenB25@users.noreply.github.com>
2023-11-20 11:45:37 -0700

    Update Scoreboard.h
    nov20

--- Scoreboard.h
+++ Scoreboard.h
@@ -1,3 +1,10 @@
+/************
+* Author: Calyn Bauer, Lauren Bailey, and Issac Jarrells
+* CS 225 Project
+* Purpose: This header helps with the cpp creates a visual of scoreboard
+* Version 1.0 Nov 11, 2023
+* Version 1.1 Nov 20, 2023
+*************/
 #ifndef SCOREBOARD_H
 #define SCOREBOARD_H
 
