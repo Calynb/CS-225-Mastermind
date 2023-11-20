251c78d2c651c8f8ca32790331638b3738c23de0
LaurenB25 <127565908+LaurenB25@users.noreply.github.com>
2023-11-20 11:32:04 -0700

    Update headers.h
    NOV20

--- headers.h
+++ headers.h
@@ -2,8 +2,12 @@
 * Author: Calyn Bauer, Lauren Bailey, and Issac Jarrells
 * CS 225 Project
 * Purpose: This header file contains all the used header files
-* Previous Version April 1, 2023
-* Version 1.0 Oct 31, 2023
+* Previous Version 1.0 April 1, 2023
+* Version 2.0 Oct 31, 2023
+* Version 2.1 Oct 19, 2023
+* Version 2.2 Nov 9, 2023
+* Version 2.3 Nov 11, 2023
+* Version 2.4 Nov 20, 2023
 *************/
 #include <iostream>
 #include <string>
