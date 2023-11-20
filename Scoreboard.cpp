94bc35b62750e62eda9642e02a72f9aea546ac4e
LaurenB25 <127565908+LaurenB25@users.noreply.github.com>
2023-11-20 11:34:38 -0700

    Update Scoreboard.cpp

--- Scoreboard.cpp
+++ Scoreboard.cpp
@@ -1,3 +1,12 @@
+/************
+* Author: Calyn Bauer, Lauren Bailey, and Issac Jarrells
+* CS 225 Project
+* Purpose: This creates a visual of scoreboard
+* Version 2.1 Oct 19, 2023
+* Version 2.2 Nov 9, 2023
+* Version 2.3 Nov 11, 2023
+* Version 2.4 Nov 20, 2023
+*************/
 #include <iostream>
 #include "headers.h"
 #include "PatternGen.h"
